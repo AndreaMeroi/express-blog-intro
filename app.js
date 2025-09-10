91b94f175fbca17e60b7567af69c732716eb78e9
AndreaMeroi <andreameroidublin@gmail.com>
2025-09-10 17:34:12 +0200

    add asset static

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
+const posts = [
 
+    {
+        titolo: 'Ciambellone',
+        content: 'quanto è buono il ciambellone',
+        img: 'immagine1',
+        tags: ['ciambella', 'dolce', 'rotondo'],
+    },
+    {
+        titolo: 'cracker',
+        content: 'quanto è buono il cracker',
+        img: 'immagine2',
+        tags: ['cracker', 'salato', 'rettangolare'],
+    },
+    {
+        titolo: 'pane_fritto',
+        content: 'quanto è buono il pane_fritto',
+        img: 'immagine3',
+        tags: ['pane_fritto', 'fritto', 'unto'],
+    },
+    {
+        titolo: 'barbabietola',
+        content: 'quanto è buona la barbabietola',
+        img: 'immagine4',
+        tags: ['barbabietola', 'rossa', 'rotsporcaondo'],
+    },
+    {
+        titolo: 'torta_paesana',
+        content: 'quanto è buona la torta_paesana',
+        img: 'immagine5',
+        tags: ['torta_paesana', 'rustica', 'paesana'],
+    },
+
+];
+
+app.get('/bacheca', (req, res) => {
+    res.json(posts)
+})
 
 
 
